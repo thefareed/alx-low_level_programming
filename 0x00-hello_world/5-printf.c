9b853487f8b030b458be4202070e8bc6f01fc4c7
YusufFareed <mamehyussuf15@gmail.com>
2023-03-16 20:09:38 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints exactly with proper grammar but the outcome is ia piece of art followed by a new line
+ * Return: 0(Sucess)
+ */
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
