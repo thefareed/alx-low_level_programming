c3a5ef631953cb4be6b9f18bf796d31d0d4dec38
YusufFareed <mamehyussuf15@gmail.com>
2023-03-16 19:32:01 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- A project that prints a name with put function
+ *Return: 0(Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
